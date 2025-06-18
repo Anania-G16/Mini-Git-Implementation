ab2be304fd36b21d57ee55265993e9a0342ddc7c
Anania <ananiagirma42@gmail.com>
2025-06-18 07:56:58 +0300

    I added the MiniGit constructor and the addFile function.

--- MiniGit.cpp
+++ MiniGit.cpp
@@ -1,1 +1,24 @@
+#include "MiniGit.hpp"
+#include <filesystem>
 
+namespace fs = filesystem;
+
+// Constructor sets up initial folders and default branch
+MiniGit::MiniGit() {
+    fs::create_directory(".minigit");
+    fs::create_directory(".minigit/objects");
+    head = nullptr;
+    current = nullptr;
+    currentBranch = "main";
+    branches[currentBranch] = nullptr;
+}
+
+// Add file to staging area (to be committed)
+void MiniGit::addFile(const string& filename) {
+    if (!fs::exists(filename)) {
+        cout << "The file does not exist."<<endl;
+        return;
+    }
+    stagingArea.push_back(filename);
+    cout << filename<< "Staged   for commit."<<endl;
+}

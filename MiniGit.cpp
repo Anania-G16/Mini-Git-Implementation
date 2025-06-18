48ed3aec8c6972b455966c8bdade8bfbd58314eb
Yeabsra Seyoum <yeabsraseyoum23@gmail.com>
2025-06-18 12:37:21 +0300

    Add getCurrentTime and commit function

--- MiniGit.cpp
+++ MiniGit.cpp
@@ -45,3 +45,41 @@ void MiniGit::addFile(const string& filename) {
     stagingArea.push_back(filename);
     cout << filename<< "Staged   for commit."<<endl;
 }
+
+// Get the current timestamp for commits
+string MiniGit::getCurrentTime() {
+    time_t now = time(nullptr);
+    stringstream ss;
+    ss << put_time(localtime(&now), "%Y-%m-%d %H:%M:%S");
+    return ss.str();
+}
+
+// Commit all staged files
+void MiniGit::commit(const string& message) {
+
+    Commit* newCommit = new Commit;
+    newCommit->message = message;
+    newCommit->timestamp = getCurrentTime();
+
+    stringstream fullContent;
+    for (const auto& file : stagingArea) {
+        ifstream inFile(file);
+        stringstream buffer;
+        buffer << inFile.rdbuf();
+        string content = buffer.str();
+        string hash = simpleHash(content);
+        saveBlob(file);  // Save a copy in object store
+        newCommit->blobs.push_back({file, hash});
+        fullContent << file << hash;  // Unique hash for commit itself
+    }
+
+    newCommit->hash = simpleHash(fullContent.str());
+    newCommit->prev = current;
+    if (current) current->next = newCommit;
+    current = newCommit;
+    if (!head) head = current;
+
+    branches[currentBranch] = current;
+    stagingArea.clear();  // Clear staged files after commit
+    cout << "Committed: " << message <<endl;
+}

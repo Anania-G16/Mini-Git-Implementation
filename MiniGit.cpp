8ba686bf49762b53b4f7de0b77b8a5e3b39202fb
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-06-18 11:30:17 +0300

    added simpleHash and saveBlop fnctions

--- MiniGit.cpp
+++ MiniGit.cpp
@@ -1,7 +1,19 @@
 #include "MiniGit.hpp"
+#include <fstream>
+#include <sstream>
+#include <iomanip>
 #include <filesystem>
 
 namespace fs = filesystem;
+
+// A basic hash function to uniquely identify content
+string simpleHash(const string& content) {
+    hash<string> hasher;
+    size_t hash = hasher(content);
+    stringstream ss;
+    ss << hex << hash;
+    return ss.str();
+}
 
 // Constructor sets up initial folders and default branch
 MiniGit::MiniGit() {
@@ -11,6 +23,17 @@ MiniGit::MiniGit() {
     current = nullptr;
     currentBranch = "main";
     branches[currentBranch] = nullptr;
+}
+
+// Save a file's contents into the .minigit/objects folder
+void MiniGit::saveBlob(const string& filename) {
+    ifstream inFile(filename);
+    stringstream buffer;
+    buffer << inFile.rdbuf();
+    string content = buffer.str();
+    string hash = simpleHash(content);
+    ofstream outFile(".minigit/objects/" + hash);
+    outFile << content;
 }
 
 // Add file to staging area (to be committed)
